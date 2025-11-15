5ce400b3c1ba79c26d4300ae2c21dc2ac6d9ef4c
hnanna2 <hnanna2@illinois.edu>
2025-11-15 03:43:02 -0600

    Adjusted web design theme and interactivity

--- app.js
+++ app.js
@@ -16,3 +16,33 @@ mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
     menuLinks.classList.toggle('active');
 });
+
+document.querySelectorAll('.event-sub').forEach(eventItem => {
+    eventItem.addEventListener('click', () => {
+        document.getElementById('modal-overlay').style.display = 'flex';
+    });
+});
+
+document.getElementById('close-btn').addEventListener('click', () => {
+    document.getElementById('modal-overlay').style.display = 'none';
+});
+
+/*keep?*/
+function openModal() {
+    modalOverlay.style.display = 'flex';
+    modalOverlay.setAttribute('aria-hidden', 'false');
+}
+
+/* adding aria label for screen readers*/
+function closeModal() {
+    modalOverlay.style.display = 'none';
+    modalOverlay.setAttribute('aria-hidden', 'true');
+}
+
+/*Escape button implementation*/ 
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+});
+
