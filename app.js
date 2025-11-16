c4c4964deaf562e72761d0de0048baafda176908
Stephanie Zhou <stephiez135@gmail.com>
2025-11-15 18:06:28 -0600

    simple filters and creation of backend branch

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
     menuLinks.classList.toggle('active');
 });
+
+const filters = document.querySelectorAll(".checkbox-wrapper-13 input")
+const filteredEvents = document.querySelectorAll(".filterable_cards .block")
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filteredEvents.forEach(block => {
+        block.classList.add("hide");
+        if(block.dataset.name == e.target.dataset.name || e.target.dataset.name == "all") {
+            block.classList.remove("hide");
+        }
+    });
+    
+};
+
+filters.forEach(input => input.addEventListener("click", filterCards));
